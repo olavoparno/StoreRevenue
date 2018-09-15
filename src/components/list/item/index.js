fb0c8cb240c5142704c6eb52094e47e7d19fb641
Olavo Parno <olavoparno@gmail.com>
2018-09-15 03:03:53 -0300

    fix(item): turn into stateless component

--- src/components/list/item/index.js
+++ src/components/list/item/index.js
@@ -1,14 +1,9 @@
 import React from "react";
 
-class Item extends React.Component {
-    componentDidMount() {
-        console.log();
-    };
-    render() {
-        return (
-            <div className={this.props.className}>{this.props.value}</div>
-        );
-    }
+const Item = (props) => {
+    return (
+        <div className={props.className}>{props.value}</div>
+    );
 }
 
 export default Item;
