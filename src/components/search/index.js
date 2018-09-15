56baac5ad9343796951f7a5cbd336df81015fe7a
Olavo Parno <olavoparno@gmail.com>
2018-09-15 02:59:17 -0300

    feat(search): implement search

--- src/components/search/index.js
+++ src/components/search/index.js
@@ -1,13 +1,44 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Search = (props) => {
-    const { defaultValue } = props;
-    return (
-        <div className="div__search">
-            <input type="text" className="div__search input__search" placeholder={defaultValue} />
-        </div>
-    );
+class Search extends React.Component {
+    constructor(props) {
+        super(props);
+        
+        const { defaultValue, data } = props;
+
+        this.state = {
+            defaultValue: defaultValue,
+            stores: data
+        };
+    }
+    
+    static getDerivedStateFromProps(nextProps, prevState) {
+        if (nextProps.data !== prevState.stores) {
+            return {
+                stores: nextProps.data.stores
+            }
+        }
+        return null;
+    }
+
+    onChange(event) {
+        const valueChanged = event.target.value;
+        this.props.onChange(valueChanged);
+    }
+
+    render() {
+        return (
+            <div className="div__search">
+                <input 
+                    type="text" 
+                    className="div__search input__search" 
+                    placeholder={this.state.defaultValue} 
+                    onChange={this.onChange.bind(this)}
+                />
+            </div>
+        );
+    }
 }
 
 Search.propTypes = {
