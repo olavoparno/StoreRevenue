633d866c2e78056c8f2033198d7242baf659106d
Olavo Parno <olavoparno@gmail.com>
2018-09-17 00:41:33 -0300

    chore(list): change list to tabular and add sorting/ordering to it

--- src/components/list/index.js
+++ src/components/list/index.js
@@ -1,8 +1,18 @@
 import React from 'react';
 
-import Item from './item';
 import Pagination from '../pagination';
 
+const TableItem = ({
+    item: { name, revenue }, className
+  }) => (
+    <tr>
+      <td>{name}</td>
+      <td className={className}>{`R$ ${revenue.toLocaleString(
+        "pt-BR",
+        { minimumFractionDigits: 2 }
+      )}`}</td>
+    </tr>
+);
 class List extends React.Component {
     constructor(props) {
         super(props);
@@ -11,17 +21,16 @@ class List extends React.Component {
 
         this.state = {
             stores: stores,
-            pageOfItems: []
+            pageOfItems: [],
+            orderedBy: true,
+            sortedBy: "name"
         };
 
         this.onChangePage = this.onChangePage.bind(this);
     }
 
     static getDerivedStateFromProps(nextProps, prevState) {
-        // console.log("props", nextProps)
-        // console.log("state ", prevState)
         if (nextProps.data !== prevState.stores) {
-            // console.log("Differente so... ", nextProps.data.stores)
             return {
                 stores: nextProps.data.stores
             }
@@ -29,10 +38,41 @@ class List extends React.Component {
         return null;
     }
 
-    componentDidUpdate(nextProps, prevState) {
-
+    componentDidUpdate() {
+    }
+    
+    componentDidMount() {
+        this.sortBy("name");
     }
 
+    sortBy(prop) {
+        const totalLength = this.state.stores.length;
+        this.setState({
+            sortedBy: prop,
+            orderedBy: !this.state.orderedBy,
+            stores: this.state.stores.sort((a, b) => {
+                if (a[prop] < b[prop]) return this.state.orderedBy ? -1 : 1;
+                if (a[prop] > b[prop]) return this.state.orderedBy ? 1 : -1;
+                return 0;
+            })
+        });
+
+        if (this.state.stores.indexOf(this.state.pageOfItems[0]) > -1 &&
+        this.state.stores.indexOf(this.state.pageOfItems[0]) > totalLength-this.state.pageOfItems.length) {
+            this.setState({
+                pageOfItems: this.state.stores.slice(0, 10)
+            })
+        } else {
+            this.setState({
+                pageOfItems: this.state.pageOfItems.sort((a, b) => {
+                    if (a[prop] < b[prop]) return this.state.orderedBy ? -1 : 1;
+                    if (a[prop] > b[prop]) return this.state.orderedBy ? 1 : -1;
+                    return 0;
+                })
+            })
+        }
+    };
+    
     onChangePage(pageOfItems) {
         this.setState({ pageOfItems: pageOfItems });
     }
@@ -41,27 +81,46 @@ class List extends React.Component {
         const { minValue } = this.props;
         return (
             <div className="div__list">
-                <div className="div__list__t">
-                    <Item className="div__item div__item__header" value="Loja" />
-                    <Item className="div__item div__item__header" value="Faturamento" />
-                </div>
-                <div className="div__list__b">
+                <table >
+                <thead>
+                    <tr>
+                    <td
+                        className={`
+                        ${this.state.sortedBy === "name" ? "isSorted" : ""}
+                        ${this.state.orderedBy ? "orderedByASC" : "orderedByDESC"}
+                        `}
+                        onClick={() => this.sortBy("name")}
+                    >
+                        loja
+                    </td>
+                    <td
+                        className={`
+                        ${this.state.sortedBy === "revenue" ? "isSorted" : ""}
+                        ${this.state.orderedBy ? "orderedByASC" : "orderedByDESC"}
+                        `}
+                        onClick={() => this.sortBy("revenue")}
+                    >
+                        faturamento
+                    </td>
+                    </tr>
+                </thead>
+                <tbody>
                     { 
-                        this.state.pageOfItems.map((store, idx) => {
-                        let className = "div__item div__item__child";
+                        this.state.pageOfItems.map(store => {
+                        let className = "td";
                         if (store.revenue < minValue) {
-                            className += " --below"
+                            className += " --below";
                         } 
                         return (
-                        <div key={idx} className="display__flex">
-                            <Item className={"div__item div__item__child"} value={store.name} />
-                            <Item className={className} value={store.revenue} />
-                        </div>)})    
+                            <TableItem className={className} key={store.name} item={store} />
+                            )
+                        })  
                     }
-                    <Pagination items={this.state.stores} onChangePage={this.onChangePage} />
-                </div>
+                </tbody>
+                </table>
+                <Pagination items={this.state.stores} onChangePage={this.onChangePage} />
             </div>
-        )
+          );
     }
 };
 
