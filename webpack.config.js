cf5895e2d721da53e7f075206fef3ec3114697d9
Olavo Parno <olavo.parno@ingresso.com>
2018-09-17 17:50:10 -0300

    chore(package.json): add copywebpack as dev dependency

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 const HtmlWebPackPlugin = require("html-webpack-plugin");
-const CopyWebpackPlugin = require('copy-webpack-plugin');
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   module: {
