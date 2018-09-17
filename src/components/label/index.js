e02c9dc1737123feb035d71c2e8eba85d9355903
Olavo Parno <olavoparno@gmail.com>
2018-09-16 23:40:56 -0300

    chore(label): add locale value

--- src/components/label/index.js
+++ src/components/label/index.js
@@ -2,21 +2,30 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const Label = (props) => {
-    const { disabled, minValue } = props;
+    let { disabled, minValue } = props;
+
+    minValue = minValue.toLocaleString(
+        "pt-BR",
+        { minimumFractionDigits: 2 }
+    )
 
     const onChange = (event) => {
-        const valueChanged = event.target.value;
+        let valueChanged = event.target.value;
+        valueChanged = valueChanged.toLocaleString(
+            "pt-BR",
+            { minimumFractionDigits: 2 }
+        )
+
         props.onChange(valueChanged);
     }
 
     return (
         <div className="div__label revenue">
             <span>Faturamento mínimo esperado</span>
-            <input type="text" 
+            <input type="number" 
                    className="div__label label__min revenue" 
                    disabled={disabled} 
                    placeholder={minValue}
-                   defaultValue={minValue}
                    onChange={onChange.bind(this)}
                 />
         </div>
